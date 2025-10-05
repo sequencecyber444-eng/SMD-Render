17a585471eefba49ded676afa992fc93531a743c
MR SHABAN <freedeployment3@gmail.com>
2025-10-05 09:37:47 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
-# SMD-Render
+![WELLCOME](Shaban/wellcome.svg)
+  
+______
+
+[![Shaban](https://raw.githubusercontent.com/iTx-Sarkar/SMD-Render/main/Shaban/mr.svg)](https://whatsapp.com/channel/0029Vb6aq4cCHDygiEqJZl0S)
+___ 
+  
+<p align="center">
+  <a href="https://whatsapp.com/channel/0029Vb6aq4cCHDygiEqJZl0S" target="_blank">
+    <img src="./Shaban/channel-update.svg" width="350" alt="Bot Updating — WhatsApp Channel | Join Fast">
+  </a>
+</p>
+
+-------------
+
+<p align="center">
+<img src="Shaban/feature-bot.svg" alt="Feature Bot" width="900"/>
+  
+<p align="center">
+<img src="Shaban/license.svg" alt="License" width="200"/>
+
+--------------
+
+ <p align="center">
+  <a href="https://github.com/iTx-Sarkar/SMD-Render/fork" target="_blank">
+    <img src="Shaban/forkstar-holo.svg" width="180" alt="Fork Star Bot Repo"/>
+  </a>
+</p>
+
+
+<p align="center">
+  <a href="https://smd-pair.zone.id/" target="_blank">
+    <img src="./Shaban/paircode-link.svg" width="195" alt="PAIR_CODE – Device Session ID">
+  </a>
+</p>
+
+-------------
+
+<div align="center">
+  <table>
+    <tr>
+      <td>
+        <a href="https://dashboard.render.com/web/new" target="_blank">
+          <img src="https://img.shields.io/badge/Render-000000?style=for-the-badge&logo=render&logoColor=white&labelColor=000000&color=FFFF00"/>
+        </a>
+      </td>
+    </tr>
+  </table>
+</div>
+
+-------------
+
+<p align="center">
+  <img src="https://i.imgur.com/LyHic3i.gif" alt="divider"/>
+</p>
+
+
+> 🔔 Stay Connected For Updates Feature Drops And Tutorials!
+
+<a href="https://youtube.com/@mrshaban282?si=ZVANSbKM0yajAbl_" target="_blank">
+  <img src="Shaban/youtube.svg" alt="YouTube" width="250"/>
+</a>
+
+<p align="center">
+  <img src="https://i.imgur.com/LyHic3i.gif" alt="footer divider"/>
+</p>
+
---
+++ config.js
@@ -0,0 +1,72 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "",
+// add your Session Id 
+AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
+// make true or false status auto seen
+AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
+// make true if you want auto reply on status 
+AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+// make true if you want auto reply on status 
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SᴇᴇN YᴏᴜʀE SᴛᴀᴛᴜS JᴜsT NᴏW Sʜᴀʙᴀɴ-Mᴅ 𓅓*",
+// set the auto reply massage on status reply
+AUTO_CHATBOT: process.env.AUTO_CHATBOT || "true", 
+// "true" = default ON, "false" = default OFF
+WELCOME: process.env.WELCOME || "false",
+// true if want welcome and goodbye msg in groups    
+ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
+// make true to know who dismiss or promoted a member in group  
+PREFIX: process.env.PREFIX || ".",
+// add your prifix for bot
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://ik.imagekit.io/mrshaban/Picsart_25-02-01_22-47-44-239.jpg",
+// add custom menu and mention reply image url   
+BOT_NAME: process.env.BOT_NAME || "SHABAN-MD V5",
+// add bot namw here for menu
+STICKER_NAME: process.env.STICKER_NAME || "Mʀ-Sʜᴀʙᴀɴ",
+// type sticker pack name 
+CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+// make this true for custum emoji react    
+CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
+// chose custom react emojis by yourself  
+OWNER_NUMBER: process.env.OWNER_NUMBER || "923059395959",
+// add your bot owner number
+OWNER_NAME: process.env.OWNER_NAME || "MR-SHABAN",
+// add bot owner name
+DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ Mʀ Sʜᴀʙᴀɴ*",
+// add bot owner name    
+ALIVE_IMG: process.env.ALIVE_IMG || "https://ik.imagekit.io/mrshaban/Picsart_25-02-01_22-47-44-239.jpg",
+// add img for alive msg 
+READ_MESSAGE: process.env.READ_MESSAGE || "false",
+// Turn true or false for automatic read msgs
+AUTO_REACT: process.env.AUTO_REACT || "false",
+// make this true or false for auto react on all msgs  
+MODE: process.env.MODE || "public",
+// make bot public-private-inbox-group 
+ANTI_LINK: process.env.ANTI_LINK || "true",
+// make anti link true,false for groups  
+AUTO_REPLY: process.env.AUTO_REPLY || "false",
+// make true or false automatic text reply 
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+// maks true for always online 
+PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
+// make false if want private mod
+AUTO_TYPING: process.env.AUTO_TYPING || "false",
+// true for automatic show typing   
+READ_CMD: process.env.READ_CMD || "false",
+// true if want mark commands as read 
+DEV: process.env.DEV || "923059395959",
+//replace with your whatsapp number        
+ANTI_CALL: process.env.ANTI_CALL || "false",
+REJECT_MSG: process.env.REJECT_MSG || "*_SOORY MY BOSS IS BUSY PLEASE DONT CALL ME_*",
+ANTI_DELETE: process.env.ANTI_DELETE || "false",
+// set true false for anti delete
+ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox", 
+// change it to 'same' if you want to resend deleted message in same chat
+AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
+// make it true for auto recoding 
+};
